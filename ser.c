e0788d6176e7925841f27a9aebfdf0f65cc0de37
ganoo914 <80074285+ganoo914@users.noreply.github.com>
2022-10-15 20:44:53 +0900

    Update ser.c

--- ser.c
+++ ser.c
@@ -1,1 +1,15 @@
 //this is small chatting server
+#include <stdio.h>
+#include <system/socket.h>
+
+int main(int argc, char *argv[])
+{
+  int socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+  
+  if (socket_descriptor == -1) 
+  {
+    printf("Couldn't create socket \n");
+  }
+  
+  return 0;
+}
